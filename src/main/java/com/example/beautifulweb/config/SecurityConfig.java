15c52ee80b9b467878994176de48ffb3b0ee4581
Nguyen19092001 <nguyen19092001@gmail.com>
2025-05-07 17:01:40 +0700

    ok

--- src/main/java/com/example/beautifulweb/config/SecurityConfig.java
+++ src/main/java/com/example/beautifulweb/config/SecurityConfig.java
@@ -25,7 +25,8 @@ public class SecurityConfig {
 
 						.requestMatchers("/login", "/resend-code", "/signup", "/forgot-password", "/reset-password",
 								"/css/**",
-								"/js/**", "/img/**", "/api/**", "/lib/**", "/manager/**", "/user/**", "/home", "/map")
+								"/js/**", "/img/**", "/api/**", "/lib/**", "/manager/**", "/user/**", "/home", "/map",
+								"/")
 						.permitAll()
 						.requestMatchers("/admin/**").hasRole("ADMIN")
 						.anyRequest().authenticated())
