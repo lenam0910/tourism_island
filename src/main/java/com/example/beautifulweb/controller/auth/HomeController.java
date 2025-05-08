38952449f2243d043435254caa537cd38c9c501d
Nguyen19092001 <nguyen19092001@gmail.com>
2025-05-08 23:39:44 +0700

    ok

--- src/main/java/com/example/beautifulweb/controller/auth/HomeController.java
+++ src/main/java/com/example/beautifulweb/controller/auth/HomeController.java
@@ -5,13 +5,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 import com.example.beautifulweb.model.TourBooking;
+import com.example.beautifulweb.model.User;
+import com.example.beautifulweb.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
+    private final UserService userService;
+
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("tourBooking", new TourBooking());
+    public String home(Model model, HttpSession session) {
+        if (session.getAttribute("userId") != null) {
+            User user = userService.getUserById((Long) session.getAttribute("userId"));
+            TourBooking tourBooking = new TourBooking();
+            tourBooking.setEmail(user.getEmail());
+
+            model.addAttribute("tourBooking", tourBooking);
+
+        } else {
+            TourBooking tourBooking = new TourBooking();
+            model.addAttribute("tourBooking", tourBooking);
+        }
+
         return "index";
     }
 }
